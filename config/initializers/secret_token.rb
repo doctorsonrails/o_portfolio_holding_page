9254b0c8b2fe6473d140c7fb95cbd5cba01d5db3
Jeremy Walker <jeremy@meducation.net>
2013-05-12 17:38:12 +0100

    Blank Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OPortfolioHoldingPage::Application.config.secret_token = '4376cc93c77a200c7391608a7627e9cc5687aa0e6bdfeca64db5d75499c116845765551f1f31331748c6a8fa9b9f22dedd37cc4c2bba85ba928d1f627a711cf3'
